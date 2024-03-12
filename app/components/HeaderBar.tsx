20775aedccf1a714fc2d883c83bef9fbfe082d52
Kevin Sutandi <kevins050324@gmail.com>
2024-03-12 18:15:56 +1100

    redesigned home page and added aceterninty

--- app/components/HeaderBar.tsx
+++ app/components/HeaderBar.tsx
@@ -1,35 +1,39 @@
 'use client';
 
 import { Dialog } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 
-const navigation = [{ name: 'Recent Works', href: '#' }];
+const navigation = [
+  { name: 'Recent Works', href: 'https://gallery.kevinsutandi.com/' },
+];
 
 export default function HeaderBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <header className='absolute inset-x-0 top-0 z-50'>
+    <header className='absolute inset-x-0 top-0 z-50 p-4'>
       <nav
-        className='flex items-center justify-between p-6 lg:px-8'
+        className='flex items-center justify-between p-6 md:px-8'
         aria-label='Global'
       >
-        <div className='flex lg:flex-1'>
+        <div className='flex md:flex-1'>
           <a href='#' className='-m-1.5 p-1.5'>
-            <span className='text-white text-2xl'>Kevin Sutandi</span>
+            <span className='text-white tracking-wider font-light text-2xl'>
+              Kevin Sutandi
+            </span>
           </a>
         </div>
-        <div className='flex lg:hidden'>
+        <div className='flex md:hidden'>
           <button
             type='button'
-            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white'
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className='sr-only'>Open main menu</span>
-            <Bars3Icon className='h-6 w-6' aria-hidden='true' />
+            <Bars2Icon className='h-6 w-6' aria-hidden='true' />
           </button>
         </div>
-        <div className='hidden lg:flex lg:gap-x-12'>
+        <div className='hidden md:flex md:gap-x-12'>
           {navigation.map((item) => (
             <a
               key={item.name}
@@ -43,7 +47,7 @@ export default function HeaderBar() {
       </nav>
       <Dialog
         as='div'
-        className='lg:hidden'
+        className='md:hidden'
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,42 @@ import HeaderBar from './components/HeaderBar';
 
 export default function Home() {
   return (
-    <div className='bg-black'>
-      <HeaderBar />
-      <div className='relative isolate px-6 pt-14 lg:px-8'>
-        <div
-          className='absolute inset-x-0 -top-36 -z-10 transform-gpu overflow-hidden'
-          aria-hidden='true'
-        >
-          <Image
-            src={CoverImage}
-            className='relative w-full h-full opacity-50 overflow-hidden'
-            alt=''
-          />
-        </div>
-        <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
-          <div className='hidden sm:mb-8 sm:flex sm:justify-center'></div>
-          <div className='text-center'>
-            <h1 className='text-lg tracking-tight text-white sm:text-3xl'>
-              Wedding, Graduation and Event Photographer
-            </h1>
+    <>
+      <div className='bg-black'>
+        <HeaderBar />
+        <div className='isolate h-screen w-full overflow-clip px-6 pt-14 lg:px-8'>
+          <div
+            className='absolute inset-x-0 -z-10 top-0 transform-gpu overflow-clip'
+            aria-hidden='true'
+          >
+            <Image
+              src={CoverImage}
+              className='opacity-50 max-h-[100vh] h-screen w-full object-cover'
+              alt=''
+            />
+          </div>
+          <div className='mx-auto h-full max-w-3xl py-32 sm:py-48 lg:py-56'>
+            <div className='hidden sm:mb-16 sm:flex sm:justify-center'></div>
+            <div className='text-center'>
+              <h1 className='tracking-wide text-white font-regular text-4xl'>
+                Wedding, Graduation and Event Photographer
+              </h1>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className='bg-white min-h-screen h-full w-full p-20'>
+        <div className=' w-2/3 mx-auto text-center'>
+          <h1 className='text-black text-4xl font-[700]'>
+            LIGHTING UP YOUR LIFE - CAPTURING MEMORIES TO LAST
+          </h1>
+        </div>
+        <div className='flex flex-col md:flex-row'>
+          <div className='text-black'>Hello</div>
+          <div className='text-black'>Hello</div>
+        </div>
+      </div>
+    </>
   );
 }
